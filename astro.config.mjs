e470d6a9a4c23c9187b45de653f1a565d4bb6565
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2024-05-25 16:46:43 +0000

    🔧 Add `cloudflare` adapter
    Signed-off-by: John Muchovej <5000729+jmuchovej@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import db from "@astrojs/db";
 import icon from "astro-icon";
+import cloudflare from "@astrojs/cloudflare";
 
 const devMode = process.env.NODE_ENV == "development";
 const port = 2364;
@@ -13,5 +14,18 @@ const port = 2364;
 export default defineConfig({
   site: devMode ? `http://localhost:${port}` : `https://academia.fyi`,
   server: { port, },
-  integrations: [icon(), tailwind(), mdx(), sitemap(), db()]
+  integrations: [
+    icon(),
+    tailwind(),
+    mdx(),
+    sitemap(),
+    db(),
+  ],
+  output: "hybrid",
+  adapter: cloudflare({
+    // https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/#local-runtime
+    // platformProxy: {
+    //   enabled: true,
+    // }
+  })
 });
